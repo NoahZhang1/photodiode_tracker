339a44d5489fda1443d6e8921e32ab4eb407c0ce
Yunnuo "Noah" Zhang <zyn920@126.com>
2022-09-22 23:01:09 -0400

    bug fix

--- script.js
+++ script.js
@@ -1,17 +1,53 @@
+// const { TimeSeries } = require("smoothie")
+
+// import { TimeSeries, SmoothieChart } from "node_modules/smoothie/smoothie.js";
+
 const socket = io('http://localhost:3000')
 
-
-socket.on('connection', socket => {
-    console.log("connected")
-
-})
+var chart = new SmoothieChart();
 
 function createTimeline() {
     var random = new TimeSeries();
     setInterval(function() {
         random.append(Date.now(), Math.random() * 10000);
     }, 500);
-    var chart = new SmoothieChart();
+    // var chart = new SmoothieChart();
     chart.addTimeSeries(random, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 4 });
     chart.streamTo(document.getElementById("chart"), 500);
 }
+
+function init() {
+    socket.on('message', data => {
+        console.log(data)
+        createTimeline()
+        // var random = new TimeSeries();
+        // setInterval(function() {
+        //     random.append(Date.now(), data)
+        // }, 500);
+        // var chart = new SmoothieChart();
+        // chart.addTimeSeries(random, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 4 });
+        // chart.streamTo(document.getElementById("chart"), 500);
+    })
+}
+
+socket.on('message', data => {
+    console.log(data)
+    createTimeline()
+    // var random = new TimeSeries();
+    // setInterval(function() {
+    //     random.append(Date.now(), data)
+    // }, 500);
+    // var chart = new SmoothieChart();
+    // chart.addTimeSeries(random, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 4 });
+    // chart.streamTo(document.getElementById("chart"), 500);
+})
+
+// function createTimeline() {
+//     var random = new TimeSeries();
+//     setInterval(function() {
+//         random.append(Date.now(), Math.random() * 10000);
+//     }, 500);
+//     var chart = new SmoothieChart();
+//     chart.addTimeSeries(random, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 4 });
+//     chart.streamTo(document.getElementById("chart"), 500);
+// }
--- server.js
+++ server.js
@@ -1,6 +1,25 @@
+// import { TimeSeries } from 'node_modules/smoothie'
+
 const io  = require('socket.io')(3000)
+
+
+// function createTimeline() {
+//     var random = new TimeSeries();
+//     setInterval(function() {
+//         random.append(Date.now(), Math.random() * 10000);
+//     }, 500);
+//     var chart = new SmoothieChart();
+//     chart.addTimeSeries(random, { strokeStyle: 'rgba(0, 255, 0, 1)', fillStyle: 'rgba(0, 255, 0, 0.2)', lineWidth: 4 });
+//     chart.streamTo(document.getElementById("chart"), 500);
+// }
+
+// var random = new TimeSeries();
+
+//generate random data 
+var rand = Math.random() * 10000
 
 io.on('connection', socket => {
     console.log("connected")
+    socket.emit('message', rand)
 
 })
